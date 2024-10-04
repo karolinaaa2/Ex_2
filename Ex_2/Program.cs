0eede607edc995ca8129099f738e57c7a93edcce
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:32:50 +0300

    Add project files.

---
+++ Ex_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть рік: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine("Рік є високосним.");
+        }
+        else
+        {
+            Console.WriteLine("Рік не є високосним.");
+        }
+    }
+}
